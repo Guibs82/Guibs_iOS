dc67939619b4508987563c4be8b9c588a61509cb
Guibs82 <805249506@qq.com>
2016-06-10 01:56:18 +0800

    C语言学习_注释
    C语言学习_注释

---
+++ C002_C_Study/C003_Annotation/main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  C003_Annotation
+//
+//  Created by 桂朝阳 on 16/6/9.
+//  Copyright © 2016年 NexTed. All rights reserved.
+//
+
+#include <stdio.h>
+
+// 单行注释 Command + /
+/*
+    多行
+    注释
+ */
+
+// 注释不参与编译
+
+// /*单行注释可以嵌套多行注释(多行注释必须在一行内)*/
+
+/*
+    // 多行注释可以嵌套单行注释
+ */
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
