e2d981def412e1bc2a7481a0510c2e59c2d51947
Guibs82 <805249506@qq.com>
2016-06-28 16:24:58 +0800

    OC学习_Property
    OC学习_Property

--- Objective-C/OC018_Property/Person.h
+++ Objective-C/OC018_Property/Person.h
@@ -34,6 +34,19 @@
     在Xcode4.4- 可以代替getter/setter方法
     编译器只要看到@property, 就知道要为属性生成对应的getter&setter的声明
     在Xcode4.5+ 可以同时生成getter&setter的声明和实现, 如果没有告诉@property要传入的参数赋值给谁, 则会默认赋值给 _参数名
+ 
+    @property弊端:
+        只会生成最简单的getter&setter, 并不会对传入的值过滤
+        如果要对传入数据进行过滤, 就必须重写getter和setter方法
+        如果不需对传入数据过滤, 则可以直接使用@property
+ 
+    如果重写了setter, 则@property只会自动生成getter
+    如果重写了getter, 则@property只会自动生成setter
+ 
+    如果利用@property来生成getter&setter, 则可以不写成员变量, 系统会自动生成 _成员变量
+    如果同时重写了getter&setter, 则@property不会自动生成 _成员变量
+ 
+    注意: @property自动帮我们生成的成员变量, 是私有的成员变量, 在.m文件中生成, 而非.h中生成的
  */
 @property int age; // 通过@property 生成的getter&setter方法名中的变量名就是此处的变量名, 所以age不需要加_
 
